ef58f96db8e13bf816bd1b81964bf5b2511c8dc4
Rob <rzhong9963@gmail.com>
2019-11-19 22:52:31 -0600

    Templates for Login System

---
+++ Egghead/Egghead/Login.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Egghead.Models;
+
+namespace Egghead
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        async void LoginButton(object sender, EventArgs e)
+        {
+            var user = new User
+            {
+                Email = email.Text,
+                Password = pass.Text
+            };
+
+            var validUser = CheckCredentials(user);
+            if (validUser)
+            {
+                
+            }
+            else
+            {
+                await DisplayAlert("Error", "Invalid Email/Password", "OK");
+                email.Text = string.Empty;
+                pass.Text = string.Empty;
+            }
+        }
+
+        async void ForgotPassword(object sender, EventArgs e)
+        {
+
+        }
+
+        async void SignUp(object sender, EventArgs e)
+        {
+
+        }
+        
+        bool CheckCredentials(User u)
+        {
+            return false; // False for now
+        }
+    }
+}
---
+++ Egghead/Egghead/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Egghead.Models
+{
+    class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string School
+        {
+            get;
+            set;
+        }
+    }
+}
