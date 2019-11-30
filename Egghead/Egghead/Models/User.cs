6c797a096731b89c83d3370d18d790178692b863
Robert Zhong <rzhong9963@gmail.com>
2019-11-29 21:05:50 -0600

    Add back Database files

---
+++ Egghead/Egghead/Models/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Egghead.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string School
+        {
+            get;
+            set;
+        }
+        public string Role
+        {
+            get;
+            set;
+        }
+        public string Bio
+        {
+            get;
+            set;
+        }
+        public byte[] Pic
+        {
+            get;
+            set;
+        } = null;
+        public string Name
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+        // City, State, Phone, Interests, Matches/Connections, Messages, etc
+    }
+}
