24e15e693c24531b35c831e82d128e17e3316731
leslyannew <leslyannew@users.noreply.github.com>
2019-12-03 07:55:43 -0600

    Add changes from Profile2 manually

---
+++ Egghead/Egghead/ProfilePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Egghead
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            new Image { Source = "ditto.jpg" };
+
+            InitializeComponent();
+        }
+    }
+}
